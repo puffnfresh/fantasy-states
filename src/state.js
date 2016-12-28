2fa33daa8de22ba4189f6272056516b62f385036
Avaq <aldwin.vlasblom@gmail.com>
2016-12-28 16:19:43 +0100

    Add comment with a link to Haskell hoist docs above hoist definition

--- src/state.js
+++ src/state.js
@@ -55,6 +55,7 @@ State.StateT = (M) => {
         });
     };
 
+    // https://hackage.haskell.org/package/mmorph-1.0.9/docs/Control-Monad-Morph.html#g:1
     StateT.hoist = (f) => (m) => {
         return StateT((s) => {
             return f(m.evalState(s)).map((x) => Tuple2(x, s));
